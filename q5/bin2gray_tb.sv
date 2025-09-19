c2a8d8fe2b0d8312a5f6d2488a47267db8abd9de
QueyingZZZ <puhao@kth.se>
2025-09-19 15:41:36 +0200

    Update bin2gray_tb.sv

--- q5/bin2gray_tb.sv
+++ q5/bin2gray_tb.sv
@@ -6,5 +6,40 @@ module bin2gray_tb;
   // Outputs
   logic [3:0] gray;
 
+  bin2gray dut(
+    .binary(binary),
+    .gray(gray)
+  );
+
+
+initial begin
+#15;
+
+  binary = 4'b0000;
+  #5;
+
+   binary = 4'b1100;
+  #5;
+   binary = 4'b0011;
+  #5;
+   binary = 4'b0110;
+  #5;
+   binary = 4'b1111;
+  #5;
+   binary = 4'b1000;
+  #5;
+   binary = 4'b0001;
+  #5;
+   binary = 4'b0111;
+  
+  #5; 
+  binary = 4'b0111;
+  #5;
+  
+
+$finish;
+end
+
+  
   // complete
 endmodule
