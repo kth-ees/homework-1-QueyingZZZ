7c622f0541245c9bf59624e2e81882ad90bec7f3
QueyingZZZ <puhao@kth.se>
2025-09-19 15:39:25 +0200

    Update count_1_tb.sv

--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps 
+
 module count_1_tb;
 
   // Inputs
@@ -6,5 +8,33 @@ module count_1_tb;
   // Outputs
   logic [2:0] out;
 
+  count_1 dut(
+    .a(a),
+    .out(out)
+  );
+
+  initial begin
+
+#15;
+    a = 4'b0000;
+#5;
+        a = 4'b0110;
+#5;
+        a = 4'b1110;
+#5;
+        a = 4'b1111;
+#5;
+        a = 4'b1000;
+#5;
+        a = 4'b0010;
+#5;
+       a = 4'b1000;
+#5;
+        a = 4'b1101;
+#5;
+     
+$finish;
+  end
+  
   // complete
 endmodule
