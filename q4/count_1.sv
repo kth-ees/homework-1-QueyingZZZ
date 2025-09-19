a30ccf8cb61f07b26374b7daa9918a1cde2e5f87
QueyingZZZ <puhao@kth.se>
2025-09-19 14:39:59 +0200

    Update count_1.sv

--- q4/count_1.sv
+++ q4/count_1.sv
@@ -2,7 +2,15 @@ module count_1 (
   input logic [3:0] a,
   output logic [2:0] out
 );
-  // …
-  // Add your description here
-  // …
+
+  always_comb begin
+  
+    out = 3'b0;
+     
+    for(int i = 0; i < 4; i++) begin
+    if(a[i]) 
+     out = out + 1'b1;
+                               end
+              end
+     
 endmodule
