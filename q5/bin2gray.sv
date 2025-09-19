6897a138f9b0552e5c80e502031a5e669e8e4998
QueyingZZZ <puhao@kth.se>
2025-09-19 15:01:00 +0200

    Update bin2gray.sv

--- q5/bin2gray.sv
+++ q5/bin2gray.sv
@@ -2,7 +2,18 @@ module bin2gray (
   input logic [3:0] binary,
   output logic [3:0] gray
 );
-  // …
-  // Add your description here
-  // …
+ 
+ always_comb begin
+ 
+
+ gray[3] = binary[3];
+ gray[2] = binary[2] ^ binary[3];
+ gray[1] = binary[1] ^ binary[2];
+ gray[0] = binary[0] ^ binary[1];
+ 
+
+ 
+ end
+ 
+ 
 endmodule
