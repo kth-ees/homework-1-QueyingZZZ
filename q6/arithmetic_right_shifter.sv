4fedfbc8cfb159277ef5a00f7d87cba2df80f637
QueyingZZZ <puhao@kth.se>
2025-09-19 15:14:54 +0200

    Update arithmetic_right_shifter.sv

--- q6/arithmetic_right_shifter.sv
+++ q6/arithmetic_right_shifter.sv
@@ -1,8 +1,15 @@
-module arithmetic_right_shifter #(parameter N) (
+module arithmetic_right_shifter #(parameter N = 5) (
   input logic [N-1:0] input_data,
-  input logic [1:0] control,
+ // input logic [1:0] control,
   output logic [N-1:0] shifted_result
 );
+
+  always_comb begin
+  
+shifted_result[N-2:0] = input_data[N-1:1];
+shifted_result[N-1]   = input_data[N-1];
+
+end
 
   // complete the module
 endmodule
