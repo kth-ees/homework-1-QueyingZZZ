0b80af8f8436a2f368dbb885f0ac3cf534573b88
QueyingZZZ <puhao@kth.se>
2025-09-19 13:14:56 +0200

    Update bin2bcd.sv

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -2,8 +2,23 @@ module bin2bcd (
   input logic [3:0] binary,
   output logic [3:0] bcd,
   output logic carry
-);
-  // …
-  // Add your description here
-  // …
+  );
+  
+always_comb begin
+
+if (binary < 4'd10) begin
+ bcd = binary;
+ carry = 1'b0;
+end
+
+else begin
+
+bcd = binary - 4'd10;
+carry = 1'b1;
+
+end
+
+end
+  
+  
 endmodule
